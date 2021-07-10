4e54cc1d87ed15ba4fd66de3a1184b1d67bb9704
DESKTOP-KGAIMJL\Hp-user <nehra7991@gmail.com>
2021-07-10 16:38:57 +0530

    8 queens problem solution Rohit

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -2,7 +2,15 @@ public class ChessBoardRenderer {
 	public boolean isBlackSquare(int square) {
 
 		// WRITE YOUR LOGIC HERE...
-		System.out.println(square);
-		return true;
+		int box = square+1;
+		if((box >= 1 && box <=8) || (box >= 17 && box <=24) || (box >= 33 && box <= 40) ||
+				(box >= 49 && box <=56)) {
+			if(box%2 == 0) return true;
+			else return false;
+		} else {
+			if(box%2 == 0) return false;
+			else return true;
+		}
+		
 	}
 }
--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -4,14 +4,22 @@ public class GameStrategy {
 
 	private int getColumn(int cellId) {
 		// WRITE YOUR LOGIC HERE...................................		
-
-		return 0;	// just for the heck of it
+		int column = cellId % 8;
+		return column;	// just for the heck of it
 	}
 	
 	private int getRow(int cellId) {
 		// WRITE YOUR LOGIC HERE....................................
-		
-		return 0;	// just for the heck of it
+		int row = 0;
+		if(cellId >=0 && cellId <= 7) row = 0;
+		else if(cellId >=8 && cellId <= 15) row = 1;
+		else if(cellId >=16 && cellId <= 23) row = 2;
+		else if(cellId >=24 && cellId <= 31) row = 3;
+		else if(cellId >=32 && cellId <= 39) row = 4;
+		else if(cellId >=40 && cellId <= 47) row = 5;
+		else if(cellId >=48 && cellId <= 55) row = 6;
+		else if(cellId >=56 && cellId <= 63) row = 7;
+		return row;	// just for the heck of it
 	}
 
 	public boolean isValidPosition(int cellId) {
@@ -29,6 +37,43 @@ public class GameStrategy {
 			WRITE YOUR LOGIC HERE...............................
 
 		*/
+		 int i, j;
+		  
+	        /* Check this row */
+	        for (i = 0; i < 8; i++)
+	            if (placedQueens[row][i] == true && i != col )
+	            	isValid = false;
+	    
+	        /* Check this column */
+	        for (i = 0; i < 8; i++)
+	            if (placedQueens[i][col] == true && i != row)
+	            	isValid = false;
+	        
+	        /* Check upper diagonal on left side */
+	        for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+	            if (placedQueens[i][j] == true)
+	            	isValid = false;
+	  
+	        /* Check lower diagonal on right side */
+	        for (i = row, j = col; i <= 7 && j <= 0; i++, j++)
+	            if (placedQueens[i][j] == true)
+	            	isValid = false;
+	        
+	        /* Check lower diagonal on left side */
+	        for (i = row, j = col; j >= 0 && i < 8; i++, j--)
+	            if (placedQueens[i][j] == true)
+	            	isValid = false;
+	        
+	        /* Check upper diagonal on right side */
+	        for (i = row, j = col; i >= 0 && j < 8; i--, j++)
+	            if (placedQueens[i][j] == true)
+	            	isValid = false;
+	       
+	        if(isValid == true) {
+	        	placedQueens[row][col] = true;
+	        	numQueens+=1;
+	        }
+		
 		return isValid;
 	}
 }
